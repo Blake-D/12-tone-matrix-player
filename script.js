3f2f819bb2dba80142d90ebfe6cea5ea88a11aa8
Blake DeGraw <bbdegraw@gmail.com>
2021-03-31 19:59:55 -0700

    P0 row populated by order input buttons. order inouts buttons delete inner text when pressed

---
+++ script.js
@@ -0,0 +1,416 @@
+let inputCount = 0
+
+let zeroClicked = false
+let oneClicked = false
+let twoClicked = false
+let threeClicked = false
+let fourClicked = false
+let fiveClicked = false
+let sixClicked = false
+let sevenClicked = false
+let eightClicked = false
+let nineClicked = false
+let tenClicked = false
+let elevenClicked = false
+
+document.getElementById('buttonZero').addEventListener('click', () => {
+    if(inputCount < 12 && zeroClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 0
+        } else if(inputCount === 1){
+            zeroOne.innerText = 0
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 0
+        } else if(inputCount === 3){
+            zeroThree.innerText = 0
+        } else if(inputCount === 4){
+            zeroFour.innerText = 0
+        } else if(inputCount === 5){
+            zeroFive.innerText = 0
+        } else if(inputCount === 6){
+            zeroSix.innerText = 0
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 0
+        } else if(inputCount === 8){
+            zeroEight.innerText = 0
+        } else if(inputCount === 9){
+            zeroNine.innerText = 0
+        } else if(inputCount === 10){
+            zeroTen.innerText = 0
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 0
+        }
+        inputCount++
+        zeroClicked = true
+        buttonZero.innerText = ""
+    }
+})
+
+document.getElementById('buttonOne').addEventListener('click', () => {
+    if(inputCount < 12 && oneClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 1
+        } else if(inputCount === 1){
+            zeroOne.innerText = 1
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 1
+        } else if(inputCount === 3){
+            zeroThree.innerText = 1
+        } else if(inputCount === 4){
+            zeroFour.innerText = 1
+        } else if(inputCount === 5){
+            zeroFive.innerText = 1
+        } else if(inputCount === 6){
+            zeroSix.innerText = 1
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 1
+        } else if(inputCount === 8){
+            zeroEight.innerText = 1
+        } else if(inputCount === 9){
+            zeroNine.innerText = 1
+        } else if(inputCount === 10){
+            zeroTen.innerText = 1
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 1
+        }
+        inputCount++
+        oneClicked = true
+        buttonOne.innerText = ""
+    }
+})
+
+document.getElementById('buttonTwo').addEventListener('click', () => {
+    if(inputCount < 12 && twoClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 2
+        } else if(inputCount === 1){
+            zeroOne.innerText = 2
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 2
+        } else if(inputCount === 3){
+            zeroThree.innerText = 2
+        } else if(inputCount === 4){
+            zeroFour.innerText = 2
+        } else if(inputCount === 5){
+            zeroFive.innerText = 2
+        } else if(inputCount === 6){
+            zeroSix.innerText = 2
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 2
+        } else if(inputCount === 8){
+            zeroEight.innerText = 2
+        } else if(inputCount === 9){
+            zeroNine.innerText = 2
+        } else if(inputCount === 10){
+            zeroTen.innerText = 2
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 2
+        }
+        inputCount++
+        twoClicked = true
+        buttonTwo.innerText = ""
+    }
+})
+
+document.getElementById('buttonThree').addEventListener('click', () => {
+    if(inputCount < 12 && threeClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 3
+        } else if(inputCount === 1){
+            zeroOne.innerText = 3
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 3
+        } else if(inputCount === 3){
+            zeroThree.innerText = 3
+        } else if(inputCount === 4){
+            zeroFour.innerText = 3
+        } else if(inputCount === 5){
+            zeroFive.innerText = 3
+        } else if(inputCount === 6){
+            zeroSix.innerText = 3
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 3
+        } else if(inputCount === 8){
+            zeroEight.innerText = 3
+        } else if(inputCount === 9){
+            zeroNine.innerText = 3
+        } else if(inputCount === 10){
+            zeroTen.innerText = 3
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 3
+        }
+        inputCount++
+        threeClicked = true
+        buttonThree.innerText = ""
+    }
+})
+
+document.getElementById('buttonFour').addEventListener('click', () => {
+    if(inputCount < 12 && fourClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 4
+        } else if(inputCount === 1){
+            zeroOne.innerText = 4
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 4
+        } else if(inputCount === 3){
+            zeroThree.innerText = 4
+        } else if(inputCount === 4){
+            zeroFour.innerText = 4
+        } else if(inputCount === 5){
+            zeroFive.innerText = 4
+        } else if(inputCount === 6){
+            zeroSix.innerText = 4
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 4
+        } else if(inputCount === 8){
+            zeroEight.innerText = 4
+        } else if(inputCount === 9){
+            zeroNine.innerText = 4
+        } else if(inputCount === 10){
+            zeroTen.innerText = 4
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 4
+        }
+        inputCount++
+        fourClicked = true
+        buttonFour.innerText = ""
+    }
+})
+
+document.getElementById('buttonFive').addEventListener('click', () => {
+    if(inputCount < 12 && fiveClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 5
+        } else if(inputCount === 1){
+            zeroOne.innerText = 5
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 5
+        } else if(inputCount === 3){
+            zeroThree.innerText = 5
+        } else if(inputCount === 4){
+            zeroFour.innerText = 5
+        } else if(inputCount === 5){
+            zeroFive.innerText = 5
+        } else if(inputCount === 6){
+            zeroSix.innerText = 5
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 5
+        } else if(inputCount === 8){
+            zeroEight.innerText = 5
+        } else if(inputCount === 9){
+            zeroNine.innerText = 5
+        } else if(inputCount === 10){
+            zeroTen.innerText = 5
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 5
+        }
+        inputCount++
+        fiveClicked = true
+        buttonFive.innerText = ""
+    }
+})
+
+document.getElementById('buttonSix').addEventListener('click', () => {
+    if(inputCount < 12 && sixClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 6
+        } else if(inputCount === 1){
+            zeroOne.innerText = 6
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 6
+        } else if(inputCount === 3){
+            zeroThree.innerText = 6
+        } else if(inputCount === 4){
+            zeroFour.innerText = 6
+        } else if(inputCount === 5){
+            zeroFive.innerText = 6
+        } else if(inputCount === 6){
+            zeroSix.innerText = 6
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 6
+        } else if(inputCount === 8){
+            zeroEight.innerText = 6
+        } else if(inputCount === 9){
+            zeroNine.innerText = 6
+        } else if(inputCount === 10){
+            zeroTen.innerText = 6
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 6
+        }
+        inputCount++
+        sixClicked = true
+        buttonSix.innerText = ""
+    }
+})
+
+document.getElementById('buttonSeven').addEventListener('click', () => {
+    if(inputCount < 12 && sevenClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 7
+        } else if(inputCount === 1){
+            zeroOne.innerText = 7
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 7
+        } else if(inputCount === 3){
+            zeroThree.innerText = 7
+        } else if(inputCount === 4){
+            zeroFour.innerText = 7
+        } else if(inputCount === 5){
+            zeroFive.innerText = 7
+        } else if(inputCount === 6){
+            zeroSix.innerText = 7
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 7
+        } else if(inputCount === 8){
+            zeroEight.innerText = 7
+        } else if(inputCount === 9){
+            zeroNine.innerText = 7
+        } else if(inputCount === 10){
+            zeroTen.innerText = 7
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 7
+        }
+        inputCount++
+        sevenClicked = true
+        buttonSeven.innerText = ""
+    }
+})
+
+document.getElementById('buttonEight').addEventListener('click', () => {
+    if(inputCount < 12 && eightClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 8
+        } else if(inputCount === 1){
+            zeroOne.innerText = 8
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 8
+        } else if(inputCount === 3){
+            zeroThree.innerText = 8
+        } else if(inputCount === 4){
+            zeroFour.innerText = 8
+        } else if(inputCount === 5){
+            zeroFive.innerText = 8
+        } else if(inputCount === 6){
+            zeroSix.innerText = 8
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 8
+        } else if(inputCount === 8){
+            zeroEight.innerText = 8
+        } else if(inputCount === 9){
+            zeroNine.innerText = 8
+        } else if(inputCount === 10){
+            zeroTen.innerText = 8
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 8
+        }
+        inputCount++
+        eightClicked = true
+        buttonEight.innerText = ""
+    }
+})
+
+document.getElementById('buttonNine').addEventListener('click', () => {
+    if(inputCount < 12 && nineClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 9
+        } else if(inputCount === 1){
+            zeroOne.innerText = 9
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 9
+        } else if(inputCount === 3){
+            zeroThree.innerText = 9
+        } else if(inputCount === 4){
+            zeroFour.innerText = 9
+        } else if(inputCount === 5){
+            zeroFive.innerText = 9
+        } else if(inputCount === 6){
+            zeroSix.innerText = 9
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 9
+        } else if(inputCount === 8){
+            zeroEight.innerText = 9
+        } else if(inputCount === 9){
+            zeroNine.innerText = 9
+        } else if(inputCount === 10){
+            zeroTen.innerText = 9
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 9
+        }
+        inputCount++
+        nineClicked = true
+        buttonNine.innerText = ""
+    }
+})
+
+document.getElementById('buttonTen').addEventListener('click', () => {
+    if(inputCount < 12 && tenClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 10
+        } else if(inputCount === 1){
+            zeroOne.innerText = 10
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 10
+        } else if(inputCount === 3){
+            zeroThree.innerText = 10
+        } else if(inputCount === 4){
+            zeroFour.innerText = 10
+        } else if(inputCount === 5){
+            zeroFive.innerText = 10
+        } else if(inputCount === 6){
+            zeroSix.innerText = 10
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 10
+        } else if(inputCount === 8){
+            zeroEight.innerText = 10
+        } else if(inputCount === 9){
+            zeroNine.innerText = 10
+        } else if(inputCount === 10){
+            zeroTen.innerText = 10
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 10
+        }
+        inputCount++
+        tenClicked = true
+        buttonTen.innerText = ""
+    }
+})
+
+document.getElementById('buttonEleven').addEventListener('click', () => {
+    if(inputCount < 12 && elevenClicked == false){
+        if(inputCount === 0){
+            zeroZero.innerText = 11
+        } else if(inputCount === 1){
+            zeroOne.innerText = 11
+        } else if(inputCount === 2){
+            zeroTwo.innerText = 11
+        } else if(inputCount === 3){
+            zeroThree.innerText = 11
+        } else if(inputCount === 4){
+            zeroFour.innerText = 11
+        } else if(inputCount === 5){
+            zeroFive.innerText = 11
+        } else if(inputCount === 6){
+            zeroSix.innerText = 11
+        } else if(inputCount === 7){
+            zeroSeven.innerText = 11
+        } else if(inputCount === 8){
+            zeroEight.innerText = 11
+        } else if(inputCount === 9){
+            zeroNine.innerText = 11
+        } else if(inputCount === 10){
+            zeroTen.innerText = 11
+        } else if(inputCount === 11){
+            zeroEleven.innerText = 11
+        }
+        inputCount++
+        elevenClicked = true
+        buttonEleven.innerText = ""
+    }
+})
+
+
+
+
+
+
