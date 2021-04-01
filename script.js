267b14b301439645e4be84650d0bdf121418aeee
Blake DeGraw <bbdegraw@gmail.com>
2021-04-01 13:17:54 -0700

    created matrix object with all prime rows initialized as empty arrays. plugged into all event listeners

--- script.js
+++ script.js
@@ -13,34 +13,50 @@ let nineClicked = false
 let tenClicked = false
 let elevenClicked = false
 
-let pitchClass = [null, null, null, null, null, null, null, null, null, null, null, null]
+let matrix = {
+    p0: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p1: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p2: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p3: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p4: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p5: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p6: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p7: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p8: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p9: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p10: [null, null, null, null, null, null, null, null, null, null, null, null],
+    p11: [null, null, null, null, null, null, null, null, null, null, null, null],
+}
 let intervalSequence = [null, null, null, null, null, null, null, null, null, null, null]
 
 function getIntervalSequence(){
-    intervalSequence[0] = pitchClass[0] - pitchClass[1]
-    intervalSequence[1] = pitchClass[0] - pitchClass[2]
-    intervalSequence[2] = pitchClass[0] - pitchClass[3]
-    intervalSequence[3] = pitchClass[0] - pitchClass[4]
-    intervalSequence[4] = pitchClass[0] - pitchClass[5]
-    intervalSequence[5] = pitchClass[0] - pitchClass[6]
-    intervalSequence[6] = pitchClass[0] - pitchClass[7]
-    intervalSequence[7] = pitchClass[0] - pitchClass[8]
-    intervalSequence[8] = pitchClass[0] - pitchClass[9]
-    intervalSequence[9] = pitchClass[0] - pitchClass[10]
-    intervalSequence[10] = pitchClass[0] - pitchClass[11]
+    intervalSequence[0] = matrix.p0[0] - matrix.p0[1]
+    intervalSequence[1] = matrix.p0[0] - matrix.p0[2]
+    intervalSequence[2] = matrix.p0[0] - matrix.p0[3]
+    intervalSequence[3] = matrix.p0[0] - matrix.p0[4]
+    intervalSequence[4] = matrix.p0[0] - matrix.p0[5]
+    intervalSequence[5] = matrix.p0[0] - matrix.p0[6]
+    intervalSequence[6] = matrix.p0[0] - matrix.p0[7]
+    intervalSequence[7] = matrix.p0[0] - matrix.p0[8]
+    intervalSequence[8] = matrix.p0[0] - matrix.p0[9]
+    intervalSequence[9] = matrix.p0[0] - matrix.p0[10]
+    intervalSequence[10] = matrix.p0[0] - matrix.p0[11]
+}
+
+function fillMatrix(){
+    console.log('hi')
 }
 
 document.getElementById('buttonZero').addEventListener('click', () => {
     if(inputCount < 12 && zeroClicked === false){
         let i = inputCount
-        pitchClass[i] = 0
+        matrix.p0[i] = 0
         inputCount++
         zeroClicked = true
         buttonZero.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -48,14 +64,13 @@ document.getElementById('buttonZero').addEventListener('click', () => {
 document.getElementById('buttonOne').addEventListener('click', () => {
     if(inputCount < 12 && oneClicked === false){
         let i = inputCount
-        pitchClass[i] = 1
+        matrix.p0[i] = 1
         inputCount++
         oneClicked = true
         buttonOne.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -63,14 +78,13 @@ document.getElementById('buttonOne').addEventListener('click', () => {
 document.getElementById('buttonTwo').addEventListener('click', () => {
     if(inputCount < 12 && twoClicked === false){
         let i = inputCount
-        pitchClass[i] = 2
+        matrix.p0[i] = 2
         inputCount++
         twoClicked = true
         buttonTwo.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -78,14 +92,13 @@ document.getElementById('buttonTwo').addEventListener('click', () => {
 document.getElementById('buttonThree').addEventListener('click', () => {
     if(inputCount < 12 && threeClicked === false){
         let i = inputCount
-        pitchClass[i] = 3
+        matrix.p0[i] = 3
         inputCount++
         threeClicked = true
         buttonThree.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -93,14 +106,13 @@ document.getElementById('buttonThree').addEventListener('click', () => {
 document.getElementById('buttonFour').addEventListener('click', () => {
     if(inputCount < 12 && fourClicked === false){
         let i = inputCount
-        pitchClass[i] = 4
+        matrix.p0[i] = 4
         inputCount++
         fourClicked = true
         buttonFour.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -108,14 +120,13 @@ document.getElementById('buttonFour').addEventListener('click', () => {
 document.getElementById('buttonFive').addEventListener('click', () => {
     if(inputCount < 12 && fiveClicked === false){
         let i = inputCount
-        pitchClass[i] = 5
+        matrix.p0[i] = 5
         inputCount++
         fiveClicked = true
         buttonFive.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -123,14 +134,13 @@ document.getElementById('buttonFive').addEventListener('click', () => {
 document.getElementById('buttonSix').addEventListener('click', () => {
     if(inputCount < 12 && sixClicked === false){
         let i = inputCount
-        pitchClass[i] = 6
+        matrix.p0[i] = 6
         inputCount++
         sixClicked = true
         buttonSix.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -138,14 +148,13 @@ document.getElementById('buttonSix').addEventListener('click', () => {
 document.getElementById('buttonSeven').addEventListener('click', () => {
     if(inputCount < 12 && sevenClicked === false){
         let i = inputCount
-        pitchClass[i] = 7
+        matrix.p0[i] = 7
         inputCount++
         sevenClicked = true
         buttonSeven.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -153,14 +162,13 @@ document.getElementById('buttonSeven').addEventListener('click', () => {
 document.getElementById('buttonEight').addEventListener('click', () => {
     if(inputCount < 12 && eightClicked === false){
         let i = inputCount
-        pitchClass[i] = 8
+        matrix.p0[i] = 8
         inputCount++
         eightClicked = true
         buttonEight.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -168,14 +176,13 @@ document.getElementById('buttonEight').addEventListener('click', () => {
 document.getElementById('buttonNine').addEventListener('click', () => {
     if(inputCount < 12 && nineClicked === false){
         let i = inputCount
-        pitchClass[i] = 9
+        matrix.p0[i] = 9
         inputCount++
         nineClicked = true
         buttonNine.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -183,14 +190,13 @@ document.getElementById('buttonNine').addEventListener('click', () => {
 document.getElementById('buttonTen').addEventListener('click', () => {
     if(inputCount < 12 && tenClicked === false){
         let i = inputCount
-        pitchClass[i] = 10
+        matrix.p0[i] = 10
         inputCount++
         tenClicked = true
         buttonTen.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
@@ -198,14 +204,13 @@ document.getElementById('buttonTen').addEventListener('click', () => {
 document.getElementById('buttonEleven').addEventListener('click', () => {
     if(inputCount < 12 && elevenClicked === false){
         let i = inputCount
-        pitchClass[i] = 11
+        matrix.p0[i] = 11
         inputCount++
         elevenClicked = true
         buttonEleven.innerText = ""
         if(inputCount === 12){
-            console.log(pitchClass)
             getIntervalSequence()
-            console.log(intervalSequence)
+            fillMatrix()
         }
     }
 })
