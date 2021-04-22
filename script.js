3391ac86d8541b7996061c18c09e01bb7d703ff5
Blake DeGraw <bbdegraw@gmail.com>
2021-04-22 15:14:28 -0700

    playback of prime row works

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const zero = new Audio("audio/0.mp3")
+const one = new Audio("audio/1.mp3")
+const two = new Audio("audio/2.mp3")
+const three = new Audio("audio/3.mp3")
+const four = new Audio("audio/4.mp3")
+const five = new Audio("audio/5.mp3")
+const six = new Audio("audio/6.mp3")
+const seven = new Audio("audio/7.mp3")
+const eight = new Audio("audio/8.mp3")
+const nine = new Audio("audio/9.mp3")
+const ten = new Audio("audio/10.mp3")
+const eleven = new Audio("audio/11.mp3")
 let inputCount = 0
 let matrixFilled = false
 
@@ -10,7 +22,38 @@ const divs = {
     rcButtons: [rcButton0, rcButton1, rcButton2, rcButton3, rcButton4, rcButton5, rcButton6, rcButton7, rcButton8, rcButton9, rcButton10, rcButton11],
     trButtons: [trButton0, trButton1, trButton2, trButton3, trButton4, trButton5, trButton6, trButton7, trButton8, trButton9, trButton10, trButton11],
     brButtons: [brButton0, brButton1, brButton2, brButton3, brButton4, brButton5, brButton6, brButton7, brButton8, brButton9, brButton10, brButton11],
-    rows: [[zero0, zero1, zero2, zero3, zero4, zero5, zero6, zero7, zero8, zero9, zero10, zero11], [one0, one1, one2, one3, one4, one5, one6, one7, one8, one9, one10, one11], [two0, two1, two2, two3, two4, two5, two6, two7, two8, two9, two10, two11], [three0, three1, three2, three3, three4, three5, three6, three7, three8, three9, three10, three11], [four0, four1, four2, four3, four4, four5, four6, four7, four8, four9, four10, four11], [five0, five1, five2, five3, five4, five5, five6, five7, five8, five9, five10, five11], [six0, six1, six2, six3, six4, six5, six6, six7, six8, six9, six10, six11], [seven0, seven1, seven2, seven3, seven4, seven5, seven6, seven7, seven8, seven9, seven10, seven11], [eight0, eight1, eight2, eight3, eight4, eight5, eight6, eight7, eight8, eight9, eight10, eight11], [nine0, nine1, nine2, nine3, nine4, nine5, nine6, nine7, nine8, nine9, nine10, nine11], [ten0, ten1, ten2, ten3, ten4, ten5, ten6, ten7, ten8, ten9, ten10, ten11], [eleven0, eleven1, eleven2, eleven3, eleven4, eleven5, eleven6, eleven7, eleven8, eleven9, eleven10, eleven11]]
+    rows: [[zero0, zero1, zero2, zero3, zero4, zero5, zero6, zero7, zero8, zero9, zero10, zero11],
+            [one0, one1, one2, one3, one4, one5, one6, one7, one8, one9, one10, one11],
+            [two0, two1, two2, two3, two4, two5, two6, two7, two8, two9, two10, two11],
+            [three0, three1, three2, three3, three4, three5, three6, three7, three8, three9, three10, three11],
+            [four0, four1, four2, four3, four4, four5, four6, four7, four8, four9, four10, four11],
+            [five0, five1, five2, five3, five4, five5, five6, five7, five8, five9, five10, five11],
+            [six0, six1, six2, six3, six4, six5, six6, six7, six8, six9, six10, six11],
+            [seven0, seven1, seven2, seven3, seven4, seven5, seven6, seven7, seven8, seven9, seven10, seven11],
+            [eight0, eight1, eight2, eight3, eight4, eight5, eight6, eight7, eight8, eight9, eight10, eight11],
+            [nine0, nine1, nine2, nine3, nine4, nine5, nine6, nine7, nine8, nine9, nine10, nine11],
+            [ten0, ten1, ten2, ten3, ten4, ten5, ten6, ten7, ten8, ten9, ten10, ten11],
+            [eleven0, eleven1, eleven2, eleven3, eleven4, eleven5, eleven6, eleven7, eleven8, eleven9, eleven10, eleven11]]
+}
+
+const audioArray = [zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven]
+
+const pitchOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+
+let playbackOrder = []
+
+function getPlaybackOrder(){
+    for(let i = 0; i < 12; i++){
+        for(let j = 0; j < 12; j++){
+            if(divs.rows[0][i].innerText === pitchOptions[j]){
+                playbackOrder[i] = audioArray[j]
+            }
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 function convertToPos(int){
@@ -54,6 +97,8 @@ document.getElementById('buttonZero').addEventListener('click', () => {
         buttonZero.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -67,6 +112,8 @@ document.getElementById('buttonOne').addEventListener('click', () => {
         buttonOne.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -80,6 +127,8 @@ document.getElementById('buttonTwo').addEventListener('click', () => {
         buttonTwo.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -93,6 +142,8 @@ document.getElementById('buttonThree').addEventListener('click', () => {
         buttonThree.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -106,6 +157,7 @@ document.getElementById('buttonFour').addEventListener('click', () => {
         buttonFour.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
         }
     }
 })
@@ -119,6 +171,8 @@ document.getElementById('buttonFive').addEventListener('click', () => {
         buttonFive.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -132,6 +186,8 @@ document.getElementById('buttonSix').addEventListener('click', () => {
         buttonSix.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -145,6 +201,8 @@ document.getElementById('buttonSeven').addEventListener('click', () => {
         buttonSeven.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -158,6 +216,8 @@ document.getElementById('buttonEight').addEventListener('click', () => {
         buttonEight.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -171,6 +231,8 @@ document.getElementById('buttonNine').addEventListener('click', () => {
         buttonNine.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -184,6 +246,8 @@ document.getElementById('buttonTen').addEventListener('click', () => {
         buttonTen.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
@@ -197,85 +261,23 @@ document.getElementById('buttonEleven').addEventListener('click', () => {
         buttonEleven.innerText = ""
         if(inputCount === 12){
             fillMatrix()
+            getPlaybackOrder()
+            console.log(playbackOrder)
         }
     }
 })
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-function play0(){
-    const audio = new Audio("audio/0.mp3")
-    audio.play()
-}
-
-function play1(){
-    const audio = new Audio("audio/1.mp3")
-    audio.play()
-}
-
-function play2(){
-    const audio = new Audio("audio/2.mp3")
-    audio.play()
-}
-
-function play3(){
-    const audio = new Audio("audio/3.mp3")
-    audio.play()
-}
-
-function play4(){
-    const audio = new Audio("audio/4.mp3")
-    audio.play()
-}
-
-function play5(){
-    const audio = new Audio("audio/5.mp3")
-    audio.play()
-}
-
-function play6(){
-    const audio = new Audio("audio/6.mp3")
-    audio.play()
-}
-
-function play7(){
-    const audio = new Audio("audio/7.mp3")
-    audio.play()
-}
-
-function play8(){
-    const audio = new Audio("audio/8.mp3")
-    audio.play()
-}
-
-function play9(){
-    const audio = new Audio("audio/9.mp3")
-    audio.play()
-}
-
-function play10(){
-    const audio = new Audio("audio/10.mp3")
-    audio.play()
-}
-
-function play11(){
-    const audio = new Audio("audio/11.mp3")
-    audio.play()
-}
-
 document.getElementById('primePlayback').addEventListener('click', () => {
-    play0()
-    sleep(300).then(() => {play1()})
-    sleep(600).then(() => {play2()})
-    sleep(900).then(() => {play3()})
-    sleep(1200).then(() => {play4()})
-    sleep(1500).then(() => {play5()})
-    sleep(1800).then(() => {play6()})
-    sleep(2100).then(() => {play7()})
-    sleep(2400).then(() => {play8()})
-    sleep(2700).then(() => {play9()})
-    sleep(3000).then(() => {play10()})
-    sleep(3300).then(() => {play11()})
+    playbackOrder[0].play()
+    sleep(300).then(() => {playbackOrder[1].play()})
+    sleep(600).then(() => {playbackOrder[2].play()})
+    sleep(900).then(() => {playbackOrder[3].play()})
+    sleep(1200).then(() => {playbackOrder[4].play()})
+    sleep(1500).then(() => {playbackOrder[5].play()})
+    sleep(1800).then(() => {playbackOrder[6].play()})
+    sleep(2100).then(() => {playbackOrder[7].play()})
+    sleep(2400).then(() => {playbackOrder[8].play()})
+    sleep(2700).then(() => {playbackOrder[9].play()})
+    sleep(3000).then(() => {playbackOrder[10].play()})
+    sleep(3300).then(() => {playbackOrder[11].play()})
 })
